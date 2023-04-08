195499127c9c9642b9bc94e3528fce9604b88694
Orest <Orest347@gmail.com>
2023-04-08 20:59:23 +0300

    task 7/2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const markup = galleryItems.map(({preview, original, description })=>` 
-
-<li 
-data-imeges="${description}"
-class=".gallery__item">
-<img class=".gallery__item" src="${preview}" alt="${description}">
-<a class=".gallery__item" href="${original}"></a>
-</li>`
-   
-);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `
+  <li data-gallery-item="${description}" class="gallery__item">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </li>
+`);
 gallery.insertAdjacentHTML('beforeend', markup.join(''));
-gallery.addEventListener('click',onClick );
-function onClick (evt,){
-    if(!evt.target.classList.contains('.gallery__item')){
-        return;
-    }
-        
-    const itemCard = evt.target.dataset.galleryItem;
-
-    const currentItem = galleryItems.find(({description}) => description === itemCard);
-            console.log(currentItem)
-}
-
+gallery.addEventListener('click', onClick);
+let currentItem;
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const itemCard = evt.target.parentNode.dataset.galleryItem;
+  currentItem = galleryItems.find(({ description }) => description === itemCard);
+  console.log(currentItem);
 const instance = basicLightbox.create(`
-<div>
-<img src="${currentItem.original}" alt="${currentItem.description}"></a>
-</div>
+<img src="${currentItem.original}" alt="${currentItem.description}">
 `)
-instance.show()
-  
 
+instance.show();
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`);
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+gallery.addEventListener('click', onClick);
+let currentItem;
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const itemCard = evt.target.parentNode.dataset.galleryItem;
+  currentItem = galleryItems.find(({ description }) => description === itemCard);
+  console.log(currentItem);
+
+
+
+  const lightbox = new SimpleLightbox('.gallery__item a',  {
+    captionsData : "${description}",
+    className: "simple-lightbox",
+  });
+  console.log(lightbox.options);
+}
+
+
+
+
