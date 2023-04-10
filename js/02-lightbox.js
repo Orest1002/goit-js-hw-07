8e363f87a3eadb76e0be8d4681c09cfbb5e808b2
Orest <Orest347@gmail.com>
2023-04-10 07:43:41 +0300

    task7 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
 const gallery = document.querySelector('.gallery');
 const markup = galleryItems.map(({ preview, original, description }) => `
 <li class="gallery__item">
@@ -11,16 +11,8 @@ const markup = galleryItems.map(({ preview, original, description }) => `
 </li>
 `);
 gallery.insertAdjacentHTML('beforeend', markup.join(''));
-gallery.addEventListener('click', onClick);
-let currentItem;
-function onClick(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  const itemCard = evt.target.parentNode.dataset.galleryItem;
-  currentItem = galleryItems.find(({ description }) => description === itemCard);
-  console.log(currentItem);
+
+ 
 
 
   const lightbox = new SimpleLightbox('.gallery a', {  
@@ -28,7 +20,7 @@ function onClick(evt) {
      captionDelay: 250,});
   
   
-}
+
 
 
 
